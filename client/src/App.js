a494af5311ac422a517b19b1b11cb2088ad21a9a
jj_ivezic <ivezic.jj@gmail.com>
2020-04-27 13:32:21 +0200

    I18n

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect,Suspense } from 'react';
 import { Router } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import { Routes } from './_routes/routes';
 import { history } from './_utils';
 
