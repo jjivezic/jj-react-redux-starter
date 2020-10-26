454dd02a1f5a2c36e9e75afba28e808b78c06870
jjivezic2 <jelena.ivezic@vivifyideas.com>
2020-10-26 12:53:15 +0100

    Redux middlewares

--- src/_store/store.js
+++ src/_store/store.js
@@ -2,13 +2,18 @@ import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
+import { url } from '../environment';
 
 const loggerMiddleware = createLogger();
 
+const middlewares = [thunkMiddleware];
+
+if (process.env.REACT_APP_ENVIROMENT !== 'prod') {
+    middlewares.push(loggerMiddleware);
+}
+
 export const store = createStore(
     rootReducer,
-    applyMiddleware(
-        thunkMiddleware,
-        loggerMiddleware
-    )
+    applyMiddleware(...middlewares),
 );
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import '../node_modules/jquery/dist/jquery.min.js';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
 import './index.scss';
 import './index.scss';
 import App from './App';
