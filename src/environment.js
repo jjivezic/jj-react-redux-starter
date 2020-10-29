c3474e04c045d1c377c14b5854f2003cc95ea84c
jjivezic2 <jelena.ivezic@vivifyideas.com>
2020-10-29 15:35:09 +0100

    ENv

--- src/environment.js
+++ src/environment.js
@@ -1,11 +1,13 @@
 
+
 const env = process.env.REACT_APP_ENVIROMENT;
 let url;
-let sourceUrl ='https://pointsnaps.s3.eu-central-1.amazonaws.com/';
-let googleApiKey='AIzaSyBFNk58EV3naa_6yU_Skpc_4l3qSWmsL50';
+let sourceUrl ='sdsdds';
+let googleApiKey='dssssssssssss';
 if (env === "prod") {
-    url = "https://pointsnaps.com/"
+    url = "https://prod..."
 } else {
     url = "http://localhost:3000/"
 }
 export { url, sourceUrl, googleApiKey };
+
