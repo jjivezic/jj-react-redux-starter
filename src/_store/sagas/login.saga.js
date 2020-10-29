02a2d5b7c20be550006b98ea5aa2ef625200640b
jjivezic2 <jelena.ivezic@vivifyideas.com>
2020-10-29 16:01:10 +0100

    Login

--- src/_store/sagas/login.saga.js
+++ src/_store/sagas/login.saga.js
@@ -2,15 +2,24 @@ import { all, call, delay, put, takeEvery } from 'redux-saga/effects'
 import { userActions } from '../actions/sagas.actions';
 import userService from '../../_services/user.service';
 import { userConstants } from "../constants";
+import { sessionService } from '../../_sessionService/storage';
+import { history } from '../../_utils';
+
 export function* login(data) {
-  console.log('Hello LOGIN!',data)
+  console.log('Hello LOGIN!', data)
   yield put(userActions.loginRequest())
-  const { response, error } = yield call(userService.login,data)
-  console.log('Response Login',response,error)
-  response ? yield put(userActions.loginSuccess(response)) : yield put(userActions.loginFailure(error));
+  const { response, error } = yield call(userService.login, data)
+  console.log('Response Login', response, error)
+  if (response) {
+    yield put(userActions.loginSuccess(response));
+    sessionService.set('user', response);
+    history.push('/app');
+  } else {
+    yield put(userActions.loginFailure(error));
+  }
 }
 export function* hello() {
-console.log('HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo')
+  console.log('HELLOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoo')
 }
 export function* watchLogin() {
   yield takeEvery(userConstants.LOGIN, login)
