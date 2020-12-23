5b265f59e8fc968fe9911139c68ae1ca1298cc53
jjivezic2 <jelena.ivezic@vivifyideas.com>
2020-12-23 10:54:18 +0100

    NavLinks with activeCalsses

--- src/_components/navbar/navbar.js
+++ src/_components/navbar/navbar.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { userActions } from "../../_store/actions/user.actions";
 import LanguageSelector from "../language/languageSelector";
-import './navbar.scss';
+import styles from './navbar.module.scss';
 
 const Navbar = (props) => {
   const { t } = useTranslation();
@@ -15,17 +15,17 @@ const Navbar = (props) => {
   };
   console.log('STORE', store)
   return (
-    <nav>
+    <nav className={styles.wrapper}>
       <h3>JJ STARTER</h3>
       <ul>
         <li>
-          <NavLink exact to="/" >{t('navbar.link.home')}</NavLink>
+          <NavLink exact to="/" activeClassName={styles.active} >{t('navbar.link.home')}</NavLink>
         </li>
         {store.auth.user && <li>
-          <NavLink to="app">{t('navbar.link.dashboard')}</NavLink>
+          <NavLink to="app" activeClassName={styles.active} >{t('navbar.link.dashboard')}</NavLink>
         </li>}
         {!store.auth.user && <li >
-          <NavLink to="login" >{t('navbar.link.login')}</NavLink>
+          <NavLink to="login" activeClassName={styles.active}>{t('navbar.link.login')}</NavLink>
         </li>}
         <li>
           {store.auth.user && <a href="#" onClick={logout}>Logout</a>}
